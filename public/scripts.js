f255121a28e9b0843c0a9d56a5ff824608ef7e2f
briannata <brianna.hy.ta@gmail.com>
2023-05-04 17:06:42 -0400

    Frontend HTML and CSS

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,28 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+function toggleRPSLS() {
+    var lizard = document.getElementsByClassName("rpsls")[0];
+    var spock = document.getElementsByClassName("rpsls")[1];
+
+    if(lizard.style.display == 'inline') {
+        lizard.style.display = 'none';
+        spock.style.display = 'none';
+    }
+    else {
+        lizard.style.display = 'inline';
+        spock.style.display = 'inline';
+    }
+}
+
+function toggleDropDown() {
+    var dropdown = document.getElementsByClassName("dropdown")[0];
+
+    if(dropdown.style.display == 'inline') {
+        dropdown.style.display = 'none';
+    }
+    else {
+        dropdown.style.display = 'inline';
+    }
+}
