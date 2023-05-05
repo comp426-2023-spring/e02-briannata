f6b2e6ec9b6f7bdad5365227cbe439dcdaac5570
briannata <brianna.hy.ta@gmail.com>
2023-05-04 20:34:00 -0400

    Backend API Calls

--- public/scripts.js
+++ public/scripts.js
@@ -37,3 +37,72 @@ function togglePopUp() {
         popup.style.display = 'inline';
     }
 }
+
+async function play() {
+    var dropdown = document.getElementsByClassName("dropdown")[0];
+    var lizard = document.getElementsByClassName("rpsls")[0];
+    var popup = document.getElementsByClassName("resultPopup")[0];
+
+    var opponent = dropdown.style.display == 'inline';
+    var rps = lizard.style.display != 'inline';
+
+    var result = document.getElementById("result");
+    var player = document.getElementById("player");
+    var opponentText = document.getElementById("opponent");
+
+    var select = document.getElementById("options");
+    var value = select.value;
+
+        if(rps) {
+            if(opponent) {
+                if(value.length > 0) {
+                    const response = await fetch(`/app/rps/play/${value}`);
+                    const jsonData = await response.json();
+                    console.log(jsonData);
+                    result.innerHTML = jsonData.result.toUpperCase();
+                    player.innerHTML = "You: " + jsonData.player;
+                    opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+                    popup.style.display = 'inline';
+                }
+            }
+            else {
+                const response = await fetch("./app/rps");
+                const jsonData = await response.json();
+                console.log(jsonData);
+                result.innerHTML = jsonData.player.toUpperCase();
+                popup.style.display = 'inline';
+            }
+        }
+        else {
+            if(opponent) {
+                if(value.length > 0) {
+                    const response = await fetch(`/app/rpsls/play/${value}`);
+                    const jsonData = await response.json();
+                    console.log(jsonData);
+                    result.innerHTML = jsonData.result.toUpperCase();
+                    player.innerHTML = "You: " + jsonData.player;
+                    opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+                    popup.style.display = 'inline';
+                }
+            }
+            else {
+                const response = await fetch("./app/rpsls");
+                const jsonData = await response.json();
+                console.log(jsonData);
+                result.innerHTML = jsonData.player.toUpperCase();
+                popup.style.display = 'inline';
+            }
+
+    }
+    
+}
+
+function reset() {
+    location.reload();
+}
+
+function startOver() {
+    var popup = document.getElementsByClassName("resultPopup")[0];
+    popup.style.display = 'none';
+    location.reload();
+}
